6d30f954b3ce8cb0d341b2cf6f4787e70a2ceda9
danricardo88 <daniel.ricardo88@outlook.com>
2023-02-23 23:59:12 -0300

    nem lembro

--- src/components/Tbody.jsx
+++ src/components/Tbody.jsx
@@ -1,28 +1,28 @@
-// import React, { useContext } from 'react';
-// import { AuthContext } from '../providers/auth';
+import React, { useContext } from 'react';
+import { AuthContext } from '../providers/auth';
 
-// export default function Tbody() {
-//   const { planetList } = useContext(AuthContext);
+export default function Tbody() {
+  const { planetList } = useContext(AuthContext);
 
-//   return (
-//     <tbody>
-//       { planetList.map((planet) => (
-//         <tr key={ planet.name }>
-//           <td>{planet.name}</td>
-//           <td>{planet.rotation_period}</td>
-//           <td>{planet.orbital_period}</td>
-//           <td>{planet.diameter}</td>
-//           <td>{planet.climate}</td>
-//           <td>{planet.gravity}</td>
-//           <td>{planet.terrain}</td>
-//           <td>{planet.surface_water}</td>
-//           <td>{planet.population}</td>
-//           <td>{planet.films}</td>
-//           <td>{planet.created}</td>
-//           <td>{planet.edited}</td>
-//           <td>{planet.url}</td>
-//         </tr>
-//       ))}
-//     </tbody>
-//   );
-// }
+  return (
+    <tbody>
+      { planetList.map((planet) => (
+        <tr key={ planet.name }>
+          <td>{planet.name}</td>
+          <td>{planet.rotation_period}</td>
+          <td>{planet.orbital_period}</td>
+          <td>{planet.diameter}</td>
+          <td>{planet.climate}</td>
+          <td>{planet.gravity}</td>
+          <td>{ planet.terrain }</td>
+          <td>{ planet.surface_water }</td>
+          <td>{planet.population}</td>
+          <td>{planet.films}</td>
+          <td>{planet.created}</td>
+          <td>{planet.edited}</td>
+          <td>{planet.url}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
